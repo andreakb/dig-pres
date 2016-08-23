49beb53603a6289c0fb3eaa7d7f844e3efb30f88
andreakb <andreakathleenbyrne@gmail.com>
2016-08-23 17:06:28 +1200

    add comments and delete obsolete fferror shell script

--- ffmpeg/fferror.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in *.*; do
-        
-       FFREPORT=file=error.log:level=24 ffprobe -loglevel warning "$file"  
-        less error.log >> errors.txt 
-      
-done 
-
--- ffmpeg/movprobe.sh
+++ ffmpeg/movprobe.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-for name in *.*; do
-        
-	ffprobe 2>/dev/null -show_format -show_streams -show_error -export_xmp 1 "$name"  > "${name%.*}_${name#*.}.txt"
-        
-done 
-
+/home/andrea/bin/movprobe.sh
